d2f864e8eb9282c85f98dec533bf0c9a981e694f
Murat Dursun <41049765+0x1dot@users.noreply.github.com>
2021-04-01 00:10:00 +0300

    Initial Commit

--- DataAccess/Abstract/IOrderDal.cs
+++ DataAccess/Abstract/IOrderDal.cs
@@ -2,13 +2,11 @@
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface ICustomerDal : IEntityRepository<Customer>
+    public interface IOrderDal : IEntityRepository<Order>
     {
-
     }
 }
